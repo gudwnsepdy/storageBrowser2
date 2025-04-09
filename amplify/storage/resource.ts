6250ec510207de994cd52cd6126f66f3671f5b57
gudwnsepdy <62203909+gudwnsepdy@users.noreply.github.com>
2025-04-09 14:25:33 +0900

    Update resource.ts
    gg

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,13 +7,6 @@ export const storage = defineStorage({
     '/*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
@@ -21,16 +14,9 @@ export const storage = defineStorage({
 export const secondaryStorage = defineStorage({
   name: 'mySecondaryStorageBucket',
    access: (allow) => ({
-    'backup_public/*': [
+    '*/*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
